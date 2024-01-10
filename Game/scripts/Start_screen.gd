e51ead879fcbe7c3c3cc52c0f8d409317f094eef
AJ <arjunajo@icloud.com>
2024-01-10 01:07:22 +0100

    Added cabin, Added more UI, Upgraded file structure
    ・Added cabin model.
    ・Organized structure of Forrest level.
    ・Added UI to show the genre with text and an icon, also added hidden UI with the genre and icon for later.
    ・Added hidden node with dark fog and light in the Forrest level, for when the game switches to horror.

--- Game/scripts/Start_screen.gd
+++ Game/scripts/Start_screen.gd
@@ -1,5 +1,6 @@
 extends Control
 
+@export var Fullscreen_on_play = true
 var Player = preload("res://Scenes/FPCC.tscn")
 
 # Called when the node enters the scene tree for the first time.
@@ -9,19 +10,21 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	get_parent().get_node("DirectionalLight3D").rotation.y = get_global_mouse_position().y * 0.001
+	get_parent().get_node("Env/DirectionalLight3D").rotation.y = get_global_mouse_position().y * 0.001
 	
 
 
 func _on_button_button_up():
-	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	if Fullscreen_on_play:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		
 	var instance = Player.instantiate()
 	get_parent().add_child(instance)
 	var playerInstance = get_parent().get_node("FPCC")
-	var sceneCam = get_parent().get_node("Camera3D")
+	var sceneCam = get_parent().get_node("TitleScreenCam")
 	
 	playerInstance.global_rotation = sceneCam.global_rotation
 	playerInstance.global_transform.origin = sceneCam.global_transform.origin
 	self.queue_free()
-	get_parent().get_node("Camera3D").queue_free()
-	get_parent().get_node("Label3D").queue_free()
+	sceneCam.queue_free()
+	get_parent().get_node("TitleScreenCam/TitleLabel").queue_free()
